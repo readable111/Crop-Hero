63429975f56db6af330f1ff90742f3d77eb5e235
Moreno, Daniel <danielmoreno3@my.unt.edu>
2024-03-22 21:30:03 -0500

    Add Work Sans font, reorganize Domine font, add basic Profile page and temp link on home to profile, add yarn to run the app on my system, add standardized colors

--- app/profile.jsx
+++ app/profile.jsx
@@ -1,12 +1,12 @@
 import { useCallBack } from 'react';
 import { StyleSheet, View, Text, Scrollable } from 'react-native'
 import { useFonts } from 'expo-font'
-import { Link } from 'expo-router';
+import Colors from '../assets/Color.js'
 
 
 const Home = () =>{ 
 	const [fontsLoaded, fontError] = useFonts({
-	'Domine-Regular': require('../assets/fonts/Domine/Domine-Regular.ttf'),
+	'WorkSans-Semibold': require('../assets/fonts/WorkSans/WorkSans-SemiBold.ttf'),
 	});
 
 	if (!fontsLoaded && !fontError) {
@@ -15,15 +15,13 @@ const Home = () =>{
 		
 	return(
 	<View style = {styles.container}>
-		<Text style = {{fontFamily: 'Domine-Regular', fontSize: 30}}>This is where the home page will go</Text>
-
-		<Link href="/profile">Link to Profile page</Link>
+		<Text style = {{fontFamily: 'WorkSans-Semibold', fontSize: 30, color: Colors.CHARCOAL}}>This is where the profile page will go</Text>
 	</View>)
 };
 
 const styles = StyleSheet.create({
 	container: {
-		backgroundColor: 0x97A5BF,
+		backgroundColor: Colors.ALMOND_TAN,
 		height: '100%',
 	}
 })
