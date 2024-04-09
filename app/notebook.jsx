3a667e3cc1b424b9801855b8a717ec7559a3d5bd
MBeard87 <158011081+MBeard87@users.noreply.github.com>
2024-04-09 16:49:58 -0500

    delete extra code notebook.jsx

--- app/notebook.jsx
+++ app/notebook.jsx
@@ -33,80 +33,8 @@ const Notebook = () =>{
 		{/*create the default phone status bar at the top of the screen*/}
 		<StatusBar backgroundColor={Colors.WHITE_SMOKE} />
 		{/*green oval at the top to denote profile picture and name*/}
-		<Text style = {styles.oval}></Text>
-		<Text style = {styles.profileName}>Daniel Moreno</Text>
-		{/*TODO: set image to display profile picture*/}
-		<Image
-			style={styles.avatarImg}
-			source = {require('../assets/ProfilePageImages/AvatarPlaceholder.png')}
-		/>
-		{/*add edit profile button*/}
-		<AppButton title="Edit Profile" specifiedStyle={styles.editProfileBtn} backgroundColor={Colors.SCOTCH_MIST_TAN} onPress={() => router.push('/profile-editprofile')}/>
-		{/*add grid of profile options*/}
-		<View style={styles.btnGridContainer}>
-			{/*row for profile settings*/}
-			<Row height={40}>
-				<Col relativeColsCovered={2} alignItems='flex-end'>
-					<AppButton title="" icon={Icons.gear_big_empty_black} specifiedStyle={styles.circle} onPress={() => router.push('/profile-settings')}/>
-				</Col>
-				<Col relativeColsCovered={8}>
-					<Text style={{fontFamily: 'WorkSans-Semibold', fontSize: 16}}>    Settings</Text>
-				</Col>
-				<Col relativeColsCovered={2}>
-					<AppButton title="" icon={Icons.arrow_right_black} specifiedStyle={styles.circle} onPress={() => router.push('/profile-settings')}/>
-				</Col>
-			</Row>
-			{/*spacer row*/}
-			<Row height={33}>
-				<Col relativeColsCovered={4}>
-					<Text></Text>
-				</Col>
-				<Col relativeColsCovered={4}>
-					<Text></Text>
-				</Col>
-				<Col relativeColsCovered={4}>
-					<Text></Text>
-				</Col>
-			</Row>
-			{/*row for billing details*/}
-			{/*info needed for billing: merchant stuff (email, business addr., company name, logo), customer name, customer email, invoice date, due date, payment terms like late fees, itemized list of goods/services, subtotal, taxes/fees/discounts, total due*/}
-			<Row height={40}>
-				<Col relativeColsCovered={2} alignItems='flex-end'>
-					<AppButton title="" icon={Icons.credit_card_black} specifiedStyle={styles.circle} onPress={() => router.push('/profile-billingdetails')}/>
-				</Col>
-				<Col relativeColsCovered={8}>
-					<Text style={{fontFamily: 'WorkSans-Semibold', fontSize: 16}}>    Billing Details</Text>
-				</Col>
-				<Col relativeColsCovered={2}>
-					<AppButton title="" icon={Icons.arrow_right_black} specifiedStyle={styles.circle} onPress={() => router.push('/profile-billingdetails')}/>
-				</Col>
-			</Row>
-			{/*spacer row*/}
-			<Row height={33}>
-				<Col relativeColsCovered={4}>
-					<Text></Text>
-				</Col>
-				<Col relativeColsCovered={4}>
-					<Text></Text>
-				</Col>
-				<Col relativeColsCovered={4}>
-					<Text></Text>
-				</Col>
-			</Row>
-			{/*row for log out*/}
-			{/*TODO: add logout functionality once accounts are added*/}
-			<Row height={40}>
-				<Col relativeColsCovered={2} alignItems='flex-end'>
-					<AppButton title="" icon={Icons.logout_icon_black} specifiedStyle={styles.circle} onPress={() => Alert.alert('Disabled until Phase 2')}/>
-				</Col>
-				<Col relativeColsCovered={8}>
-					<Text style={{fontFamily: 'WorkSans-Semibold', fontSize: 16}}>    Log Out</Text>
-				</Col>
-				<Col relativeColsCovered={2}>
-					<AppButton title="" icon={Icons.arrow_right_black} specifiedStyle={styles.circle} onPress={() => Alert.alert('Disabled until Phase 2')}/>
-				</Col>
-			</Row>
-		</View>
+		
+		
 	</View>
 	)
 };
