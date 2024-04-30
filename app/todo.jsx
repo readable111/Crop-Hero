f9130c9836050db5aa808ac4f9b530aad8afbe6f
Moreno, Daniel <danielmoreno3@my.unt.edu>
2024-04-30 11:11:58 -0500

    Made a bunch of changes to the formatting of the todo page

--- app/todo.jsx
+++ app/todo.jsx
@@ -1,3 +1,6 @@
+{/*McKenna Beard for IT Capstone 2024  UNT To-do Page as apart of the notebook tab on the nav bar*/ }
+{/*This page can only be accessed after clicking on the notebook page from the main nav bar*/ }
+
 import { React, useState } from 'react';
 import {
 	StyleSheet,
@@ -13,19 +16,21 @@ import Colors from '../assets/Color.js'
 import AppButton from '../assets/AppButton.jsx'
 import { Input } from 'react-native-elements'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { CheckBox } from '@rneui/themed';
 import DropDownPicker from 'react-native-dropdown-picker'
 import NavBar from '../assets/NavBar.jsx'
 
 const todo = () => {
 	const [index, setIndex] = useState(0); //constant for tabs
-	const [checked, setChecked] = useState([false, false]); // constant for checked to-do list under ListItems from RNE
+	const [checked, setChecked] = useState(true);
+	const toggleCheckbox = () => setChecked(!checked);
 	//const [expanded, setExpanded] = useState(false); // for accordian expansion
 	{/*constants for date input*/ }
 	//const mask = '[00]{-}[00]{-}[0000]'
 	{/*Constants for icon drop down menu*/ }
 	const [items, setItems] = useState([ //potential subscription model stuff
 		{ label: '', value: 'watering-can', icon: () => <MaterialCommunityIcons name="watering-can" size={40} color="blue" /> },  // watering task
-		{ label: '', value: 'calender-clock', icon: () => <MaterialCommunityIcons name="calendar-clock" size={40} color="black" /> }, // planning task tag
+		{ label: '', value: 'calender', icon: () => <MaterialCommunityIcons name="calendar" size={40} color="black" /> }, // planning task tag
 		{ label: '', value: 'rake', icon: () => <MaterialCommunityIcons name="rake" size={40} color={Colors.IRISH_GREEN} /> },  // rake task tag icon
 		{ label: '', value: 'shovel', icon: () => <MaterialCommunityIcons name="shovel" size={40} color="black" /> },  // shovel for digging task icon
 		{ label: '', value: 'tools', icon: () => <MaterialCommunityIcons name="tools" size={40} color="black" /> },   // tool icon tag for needed to build/fix something
@@ -54,6 +59,10 @@ const todo = () => {
 	//place holder variables for todo tasks when testing
 	initialFirstName = ""
 	givenDate = ''
+	taskOne = ""
+	taskTwo = ""
+	taskThree = ""
+	taskTitle = ""
 	//givenDate=""
 	{/*TODO: add dark mode*/ }
 	{/*return the page view with all of its contents*/ }
@@ -62,9 +71,9 @@ const todo = () => {
 		//const renderRow = ({ item }: { item }) => {
 	return (
 		<View style={styles.topContainer}>
-			{/*create the default phone status bar at the top of the screen*/}
+			{/*create the default phone status bar at the top of the screen-------------------------------------*/}
 			<StatusBar backgroundColor={Colors.WHITE_SMOKE} />
-			{/*top row of buttons*/}
+			{/*Button Grid for switching between Notebook and todo pages---------------------------------------*/ }
 			<View style={styles.btnGridContainer}>
 				{/*row for profile settings*/}
 				<Row height={80}>
@@ -75,99 +84,191 @@ const todo = () => {
 
 					</Col>
 					<Col relativeColsCovered={2} alignItems='center'>
+						{/*Button to switch to notebook page*/ }
 						<AppButton title="Notebook" specifiedStyle={styles.ovals} onPress={() => router.push('/notebook')} />
 					</Col>
 				</Row>
 
 
 			</View>
+			{/*start of scrolling section of the page---------------------------------------------------------------------------------*/ }
 			<ScrollView style={styles.scroll}>
-				<View style={styles.fstContainer}>
-					{/**trying this-dropdown picker for icon selection*/}
-					<DropDownPicker
-						open={open}
-						value={value}
-						items={items}
-						setOpen={setOpen}
-						setValue={setValue}
-						setItems={setItems}
-						disableBorderRadius={true}
-						listMode='SCROLLVIEW'
-						backgroundColor={Colors.SCOTCH_MIST_TAN }
-						dropDownDirection='BOTTOM'
-						showTickIcon={false}
-						props={{
-							activeOpacity: 1,
-						}}
-						scrollViewProps={{
-							nestedScrollEnabled: false,
-							borderColor: 'clear'
-						}}
-						labelStyle={{
-							fontFamily: 'WorkSans-Regular',
-							fontSize: 16,
-						}}
-						listItemLabelStyle={{
-							fontFamily: 'WorkSans-Regular',
-							fontSize: 16,
-						}}
-						containerStyle={{
-							width: '23%',
-							//marginTop: 58,
-							zIndex: 900,
-							marginBottom: 50,
-							//marginLeft: 260,
-							//marginTop: 120
-							alignSelf: 'flex-end',
-							marginHorizontal: 7,
-							//marginBottom: -75,
-							//outerHeight: 25
-							backgroundColor: 'clear',
-							borderColor: 'clear'
-						}}
-						dropDownContainerStyle={{
-							borderWidth: 0,
-							//borderColor: 'black',
-							//borderRadius: 5,
-							zIndex: 900,
-							backgroundColor: 'clear',
-							borderColor: 'clear'
+				<View style={styles.fstTryContainer}>
+						{/**trying this-dropdown picker for icon selection*/}
+						<DropDownPicker
+							open={open}
+							value={value}
+							items={items}
+							setOpen={setOpen}
+							setValue={setValue}
+							setItems={setItems}
+							disableBorderRadius={true}
+							listMode='SCROLLVIEW'
+							backgroundColor={Colors.SCOTCH_MIST_TAN }
+							dropDownDirection='BOTTOM'
+							showTickIcon={false}
 							
-						}}
-						style={{
-							//borderColor: 'black',
-							borderWidth: 0,
-							//borderRadius: 5,
-							//height: 40,
-							backgroundColor: 'clear',
-							borderColor: 'clear'
-						}}
-					/>
-					<Input
-						//rightIcon={<AntDesign name="edit" size={24} color="black" />}
-						inputContainerStyle={styles.inputBox}
-						inputStyle={styles.inputBoxStyle}
-						selectionColor={Colors.SANTA_GRAY}
-						placeholder='Things to schedule'
-						defaultValue={initialFirstName}
-						autoComplete='name'
-						maxLength={256}
-						multiline={true}
-						textAlign="flex-start"
-					/>
+							props={{
+								activeOpacity: 1,
+							}}
+							scrollViewProps={{
+								nestedScrollEnabled: true,
+								borderColor: 'clear',
+								zIndex: 1000,
+							}}
+							labelStyle={{
+								fontFamily: 'WorkSans-Regular',
+								fontSize: 16,
+							}}
+							listItemLabelStyle={{
+								fontFamily: 'WorkSans-Regular',
+								fontSize: 16,
+							}}
+							containerStyle={{
+								width: '20%',
+								zIndex: 900,
+								marginBottom: -80,
+								alignSelf: 'flex-end',
+								marginHorizontal: 20,
+								backgroundColor: Colors.SCOTCH_MIST_TAN,
+								borderColor: 'clear'
+							}}
+							dropDownContainerStyle={{
+								borderWidth: 0,
+								zIndex: 900,
+								backgroundColor: Colors.SCOTCH_MIST_TAN,
+								borderColor: 'clear',
+								
+							}}
+							style={{
+								borderWidth: 0,
+								backgroundColor: Colors.SCOTCH_MIST_TAN,
+								borderColor: 'clear',
+							}}
+						/>
+						<Input
+							inputContainerStyle={styles.inputBox}
+							inputStyle={styles.inputBoxStyle}
+							selectionColor={Colors.SANTA_GRAY}
+							placeholder='Things to do'
+							defaultValue={taskTitle}
+							maxLength={256}
+							multiline={true}
+							textAlign="flex-start"
+						/>
+						
 					{/* checkboxes for to-do list*/}
 					<View style={styles.checkBoxFormat}>
-						
+						<Row height={100}>
+
+							<Col relativeColsCovered={1} alignItems='left'>
+								{/* <Text style={styles.pageTitle}>Settings</Text>*/}
+								{/*Button to switch to To-do page*/}
+								<CheckBox
+									checked={check1}
+									onPress={() => setCheck1(!check1)}
+									iconType="material-community"
+									checkedIcon="checkbox-outline"
+									uncheckedIcon={'checkbox-blank-outline'}
+									checkedColor={Colors.IRISH_GREEN}
+									uncheckedColor='black'
+									backgroundColor='clear'
+									containerStyle={styles.checkBoxContainer }
+								/>
+
+							</Col>
+							<Col relativeColsCovered={3} alignItems='center'>
+								<Input
+									inputContainerStyle={styles.inputBoxCheck}
+									inputStyle={styles.inputBoxStyleTry}
+									selectionColor={Colors.SANTA_GRAY}
+									placeholder='Things to schedule'
+									defaultValue={taskOne}
+									autoComplete='name'
+									maxLength={256}
+									multiline={true}
+									textAlign="flex-start"
+								/>
+							</Col>
+						</Row>
+						<Row height={100}>
+
+							<Col relativeColsCovered={1} alignItems='left'>
+								{/* <Text style={styles.pageTitle}>Settings</Text>*/}
+								{/*Button to switch to To-do page*/}
+								<CheckBox
+									checked={check2}
+									onPress={() => setCheck2(!check2)}
+									iconType="material-community"
+									checkedIcon="checkbox-outline"
+									uncheckedIcon={'checkbox-blank-outline'}
+									checkedColor={Colors.IRISH_GREEN}
+									uncheckedColor='black'
+									backgroundColor='clear'
+									containerStyle={styles.checkBoxContainer}
+								/>
+
+							</Col>
+							<Col relativeColsCovered={3} alignItems='center'>
+								<Input
+
+									inputContainerStyle={styles.inputBoxCheck}
+									inputStyle={styles.inputBoxStyleTry}
+									selectionColor={Colors.SANTA_GRAY}
+									placeholder='Things to schedule'
+									defaultValue={taskTwo}
+									autoComplete='name'
+									maxLength={256}
+									multiline={true}
+									textAlign="flex-start"
+								/>
+							</Col>
+						</Row>
+						<Row height={100}>
+
+							<Col relativeColsCovered={1} alignItems='left'>
+								{/* <Text style={styles.pageTitle}>Settings</Text>*/}
+								{/*Button to switch to To-do page*/}
+								<CheckBox
+									checked={check3}
+									onPress={() => setCheck3(!check3)}
+									iconType="material-community"
+									checkedIcon="checkbox-outline"
+									uncheckedIcon={'checkbox-blank-outline'}
+									checkedColor={Colors.IRISH_GREEN}
+									uncheckedColor='black'
+									backgroundColor='clear'
+									containerStyle={styles.checkBoxContainer}
+								/>
+
+							</Col>
+							<Col relativeColsCovered={3} alignItems='left'>
+								<Input
+									inputContainerStyle={styles.inputBoxCheck}
+									inputStyle={styles.inputBoxStyleTry}
+									selectionColor={Colors.SANTA_GRAY}
+									placeholder='Things to schedule'
+									defaultValue={taskThree}
+									autoComplete='name'
+									maxLength={256}
+									multiline={true}
+									textAlign="flex-start"
+								/>
+							</Col>
+						</Row>
+
 					</View>
+					{/* Button below is the edit icon which will open up the text input features in semester 2*/ }
 					<AppButton specifiedStyle={{ marginTop: 0, zIndex: 5, alignItems: "flex-end" }} title="" ad="edit" adSize={24} adColor="black" onPress={() => Alert.alert('Icon Button pressed')} />
 				</View>
+				{/*Will be try to use rows and coloumns to organize box 2x2-----------------------------------------------------------------------------*/}
 				<View style={styles.fstContainer}>
 					<Input
 						//rightIcon={<AntDesign name="edit" size={24} color="black" />}
 						inputContainerStyle={styles.inputBox}
 						inputStyle={styles.inputBoxStyle}
 						selectionColor={Colors.SANTA_GRAY}
-						placeholder='What happened today?'
+						placeholder='Things to do'
 						defaultValue={initialFirstName}
 						autoComplete='name'
 						maxLength={256}
@@ -178,11 +279,10 @@ const todo = () => {
 				</View>
 				<View style={styles.fstContainer}>
 					<Input
-						//rightIcon={<AntDesign name="edit" size={24} color="black" />}
 						inputContainerStyle={styles.inputBox}
 						inputStyle={styles.inputBoxStyle}
 						selectionColor={Colors.SANTA_GRAY}
-						placeholder='What happened today?'
+						placeholder='Things to do'
 						defaultValue={initialFirstName}
 						autoComplete='name'
 						maxLength={256}
@@ -193,11 +293,10 @@ const todo = () => {
 				</View>
 				<View style={styles.fstContainer}>
 					<Input
-						//rightIcon={<AntDesign name="edit" size={24} color="black" />}
 						inputContainerStyle={styles.inputBox}
 						inputStyle={styles.inputBoxStyle}
 						selectionColor={Colors.SANTA_GRAY}
-						placeholder='What happened today?'
+						placeholder='Things to do'
 						defaultValue={initialFirstName}
 						autoComplete='name'
 						maxLength={256}
@@ -208,11 +307,10 @@ const todo = () => {
 				</View>
 				<View style={styles.fstContainer}>
 					<Input
-						//rightIcon={<AntDesign name="edit" size={24} color="black" />}
 						inputContainerStyle={styles.inputBox}
 						inputStyle={styles.inputBoxStyle}
 						selectionColor={Colors.SANTA_GRAY}
-						placeholder='What happened today?'
+						placeholder='Things to do'
 						defaultValue={initialFirstName}
 						autoComplete='name'
 						maxLength={256}
@@ -223,11 +321,10 @@ const todo = () => {
 				</View>
 				<View style={styles.fstContainer}>
 					<Input
-						//rightIcon={<AntDesign name="edit" size={24} color="black" />}
 						inputContainerStyle={styles.inputBox}
 						inputStyle={styles.inputBoxStyle}
 						selectionColor={Colors.SANTA_GRAY}
-						placeholder='What happened today?'
+						placeholder='Things to do'
 						defaultValue={initialFirstName}
 						autoComplete='name'
 						maxLength={256}
@@ -244,33 +341,58 @@ const todo = () => {
 
 {/*define all of the custom styles for this page*/ }
 const styles = StyleSheet.create({
+	checkBoxContainer: {
+		backgroundColor: Colors.SCOTCH_MIST_TAN,
+		paddingBottom: 30,
+    },
 	fstContainer: {
 		width: '90%',
 		backgroundColor: Colors.SCOTCH_MIST_TAN,
-		//alignItems: "center",
 		padding: 5,
-		//paddingVertial: 50,
 		marginBottom: 27,
 		marginTop: 20,
-		//paddingTop:40
 		border: 'black',
 		borderWidth: 1,
 		borderRadius: 5,
 		alignSelf: 'center'
 	},
-	scroll: {
-		//flex: 1,
-		//marginTop: 20,
-		width: '100%'
-	},
+	fstTryContainer: {
+		width: '90%',
+		backgroundColor: Colors.SCOTCH_MIST_TAN,
+		padding: 5,
+		marginBottom: 5,
+		marginTop: 10,
+		border: 'black',
+		borderWidth: 1,
+		borderRadius: 5,
+		alignSelf: 'center',
+		height: 'auto'
 
+    },
+	gridContainerInput: {
+		flex: 2,
+		backgroundColor: Colors.SCOTCH_MIST_TAN,
+		border: 2,
+		borderRadius: 5,
+		borderColor: 'black',
+		width: '90%',
+		alignSelf: 'center'
+    },
+	checkBoxFormat: {
+		width: '90%',
+		border: 1,
+		borderRadius: 5,
+		backgroundColor: Colors.SCOTCH_MIST_TAN,
+    },
+	scroll: {
+		width: '100%',
+		height: '100%'
+	},
 	topContainer: {
 		backgroundColor: Colors.PERIWINKLE_GRAY,
-		//height: '100%',
 		flex: 1,
 		alignItems: 'flex-start',
 		flexDirection: 'column',
-		//marginBottom: '145%',
 		zIndex: -1,
 		height: 20
 	},
@@ -281,70 +403,68 @@ const styles = StyleSheet.create({
 		borderColor: 'black',
 		overflow: 'hidden',
 		borderBottomWidth: 2,
-		//marginTop: -18,
 		fontFamily: 'WorkSans-Regular',
 		fontSize: 16,
-		//paddingLeft: 10,
-		//marginBottom: 0,
 		zIndex: 5,
 		flex: 3,
 		marginTop: 35,
 		marginBottom: -15, 
-		width: '70%'
+		width: '70%',
+		paddingLeft: 5,
 	},
 	inputDateStyle: {
 		paddingBottom: 20,
-
+	},
+	inputBoxStyleTry: {
+		fontFamily: 'WorkSans-Regular',
+		fontSize: 16,
+		color: 'black',
+		paddingLeft: 1,
+		height: '80%',
+		padding: 2,
+	},
+	inputBoxStyleCheck: {
+		fontFamily: 'WorkSans-Regular',
+		fontSize: 16,
+		color: 'black',
+		height: '80%',
+		width: '50%',
 	},
 	inputBoxStyle: {
 		fontFamily: 'WorkSans-Regular',
 		fontSize: 16,
 		color: 'black',
 		paddingLeft: 1,
-		//height: 20
-		padding: 2
+		padding: 2,
+		height: '80%',
 	},
-
 	oval: {
 		backgroundColor: Colors.IRISH_GREEN,
 		width: 180,
 		height: 180,
 		borderRadius: 180 / 2, //borderRadius cannot exceed 50% of width or React-Native makes it into a diamond
-		//transform: [{ scaleX: 2 }],
-		//marginTop: -250,
 		paddingTop: 120,
 		marginTop: -90,
 		fontSize: 18,
 		textAlign: 'center',
 		fontFamily: 'Domine-Regular',
-		//marginBottom: 20
-
 	},
 	ovals: {
 		backgroundColor: Colors.ALMOND_TAN,
 		width: 180,
 		height: 180,
 		borderRadius: 180 / 2, //borderRadius cannot exceed 50% of width or React-Native makes it into a diamond
-		//transform: [{ scaleX: 2 }],
-		//marginTop: -250,
 		paddingTop: 120,
 		marginTop: -90,
 		fontSize: 18,
 		textAlign: 'center',
 		fontFamily: 'Domine-Regular',
-		//marginBottom: 20
-
 	},
-
-
-
 	profileName: {
 		color: 'white',
 		fontSize: 36,
 		fontFamily: 'Domine-Medium',
-		//marginTop: -130,
 	},
-
 	editProfileBtn: {
 		fontSize: 16,
 		color: "black",
@@ -356,17 +476,11 @@ const styles = StyleSheet.create({
 		//flex: -1, // # of columns
 		marginHorizontal: "auto",
 		width: '100%',
-		//marginTop: 12,
 		backgroundColor: Colors.ALMOND_TAN,
 		borderRadius: 5,
 		borderColor: 'black',
 		borderWidth: 1,
-		//marginBottom: -20,
 		height: '13%'
-		//height: 20,
-
-
-
 	}
 })
 
