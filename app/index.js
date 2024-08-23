c9b9960f2581f51f73838f209966ed72916468ac
readable111 <bowent111@gmail.com>
2024-08-23 09:11:34 -0500

    connecting the database and working on auth system

--- app/index.js
+++ app/index.js
@@ -1,6 +1,14 @@
 import Home from './home.jsx'
-
+import React { useEffect, useState } from 'react'
 export default function Page() {
+
+  const [ backendData, getBackendData ] = useState()
+
+  useEffect({
+    fetch.get('/connect').then(response=>response.json()
+    ).then(d=>console.log(d)
+  })
+
   return (
     //<View style={styles.container}>
       //<View style={styles.main}>
---
+++ server/server.js
@@ -0,0 +1,57 @@
+var mysql = require('mysql')
+const express = require('express')
+const fs = require('fs')
+const crypto = require('crypto')
+const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
+
+const app = express() 
+
+const jwtCheck = auth({
+  audience: 'https://cropauth',
+  issuerBaseURL: 'https://dev-00e0exzucdhwq0a0.us.auth0.com/',
+  tokenSigningAlg: 'RS256'
+});
+
+app.use(jwtCheck)
+var connection=mysql.createConnection({
+  host:"cropdev.mysql.database.azure.com", 
+  user:"tjb0264", 
+  password:"CropALly!", 
+  database:"db_cropdev", 
+  port:3306, 
+  ssl:{
+    ca:fs.readFileSync("../DigiCertGlobalRootCA.crt.pem")
+    }
+});
+
+connection.connect();
+app.get('/cropauth/public', function (req, res) {
+    res.send('Secured Resource');
+});
+
+app.post('./signup',(req,res)=>{
+  var email = req.body.email
+  var password = req.body.password
+  
+  
+  connection.query("SELECT fld_s_email, fld_s_Password, fld_s_PassSalt FROM tbl_subscribers", (error, results, fields)=>{
+    results.forEach(element => {  
+      async 
+    });
+
+  })
+
+})
+
+ /* 
+app.get('/connect', (req, res) =>{
+
+  connection.query('SELECT * FROM tbl_subscribers;', (error, results, fields)=>{
+    if (error) throw error;
+    res.json(results) 
+  })
+  res.status(200)
+  res.send()
+})*/
+
+app.listen(3000)
