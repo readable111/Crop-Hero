295ad20ae58ab1b2fd87d125a6a9a7a44ea40d5a
MBeard87 <158011081+MBeard87@users.noreply.github.com>
2024-10-02 15:03:06 -0500

    Update notebook.jsx

--- app/notebook.jsx
+++ app/notebook.jsx
@@ -1,4 +1,4 @@
-{/*McKenna Beard for IT Capstone 2024  UNT Notebook Page as apart of the notebook tab on the nav bar--last updated 4_30_2024*/ }
+{/*McKenna Beard for IT Capstone 2024  UNT Notebook Page as apart of the notebook tab on the nav bar--last updated 10_2_2024*/ }
 {/*This  page is meant to keep track of what was done that day for future reference if needed*/ }
 
 import { React, useState } from 'react';
