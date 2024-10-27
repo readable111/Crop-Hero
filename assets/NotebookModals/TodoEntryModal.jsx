adfea33b769c5573c4f0ecc8f7d457ef519ec8a5
MBeard87 <158011081+MBeard87@users.noreply.github.com>
2024-10-27 17:15:53 -0500

    Update TodoEntryModal.jsx

--- assets/NotebookModals/TodoEntryModal.jsx
+++ assets/NotebookModals/TodoEntryModal.jsx
@@ -1,5 +1,4 @@
 //modal pop for task entry
-// McKenna Beard
 import React, { useEffect, useState } from 'react';
 import {
     Modal,
@@ -46,6 +45,7 @@ const TodoEntryModal = ({ visible, onClose, onSave, taskID, farmers, locations, 
 
     // Function to handle save
     const handleSave = () => {
+        console.log("Modal Save Pressed:", taskData);
         onSave(taskData);
         onClose();
     };
@@ -68,50 +68,55 @@ const TodoEntryModal = ({ visible, onClose, onSave, taskID, farmers, locations, 
                     <View style={styles.attributesContainer }>
                 <Text style={styles.title}>Task Details</Text>
 
-                <Text>Task ID</Text>
-                <TextInput
-                    value={taskData.TaskID ? taskData.TaskID.toString() : ''}
-                    editable={false} // Make TaskID non-editable
-                    style={styles.input}
-                />
-
-                <Text>Assigned Farmer</Text>
-                <Picker
-                    selectedValue={taskData.AssignedFarmerID}
-                    onValueChange={value => handleChange('AssignedFarmerID', value)}
-                    style={styles.picker}
-                >
-                    <Picker.Item label="Select Farmer" value="" />
-                    {farmers.map(farmer => (
-                        <Picker.Item key={farmer.id} label={farmer.name} value={farmer.id} />
-                    ))}
-                </Picker>
-
-                <Text>Location</Text>
-                <Picker
-                    selectedValue={taskData.LocationID}
-                    onValueChange={value => handleChange('LocationID', value)}
-                    style={styles.picker}
-                >
-                    <Picker.Item label="Select Location" value="" />
-                    {locations.map(location => (
-                        <Picker.Item key={location.id} label={location.name} value={location.id} />
-                    ))}
-                </Picker>
-
-                <Text>Crop</Text>
-                <Picker
-                    selectedValue={taskData.CropID}
-                    onValueChange={value => handleChange('CropID', value)}
-                    style={styles.picker}
-                >
-                    <Picker.Item label="Select Crop" value="" />
-                    {crops.map(crop => (
-                        <Picker.Item key={crop.id} label={crop.name} value={crop.id} />
-                    ))}
-                </Picker>
-
-                <Text>Task Type</Text>
+                <View style={styles.totalTaskContainer }>    
+                    <Text>Task ID</Text>
+                    <TextInput
+                        value={taskData.TaskID ? taskData.TaskID.toString() : ''}
+                        editable={false} // Make TaskID non-editable
+                        style={styles.taskIDStyle}
+                    />
+                </View>
+                <View style={styles.totalTaskContainer}>
+                    <Text style={styles.PickerTitles }>Assigned Farmer</Text>
+                    <Picker
+                        selectedValue={taskData.AssignedFarmerID}
+                        onValueChange={value => handleChange('AssignedFarmerID', value)}
+                        style={styles.picker}
+                    >
+                                <Picker.Item label="Select Farmer" value="" style={styles.pickerItemStyle } />
+                        {farmers.map(farmer => (
+                            <Picker.Item key={farmer.id} label={farmer.name} value={farmer.id} />
+                        ))}
+                    </Picker>
+                        </View>
+                <View style={styles.totalTaskContainer}>
+                    <Text style={styles.PickerTitles}>Location</Text>
+                    <Picker
+                        selectedValue={taskData.LocationID}
+                        onValueChange={value => handleChange('LocationID', value)}
+                        style={styles.picker}
+                    >
+                                <Picker.Item label="Select Location" value="" style={styles.pickerItemStyle} />
+                        {locations.map(location => (
+                            <Picker.Item key={location.id} label={location.name} value={location.id} />
+                        ))}
+                    </Picker>
+                        </View>
+                <View style={styles.totalTaskContainer}>
+                    <Text style={styles.PickerTitles}>Crop</Text>
+                    <Picker
+                        selectedValue={taskData.CropID}
+                        onValueChange={value => handleChange('CropID', value)}
+                        style={styles.picker}
+                    >
+                        <Picker.Item label="Select Crop" value="" />
+                        {crops.map(crop => (
+                            <Picker.Item key={crop.id} label={crop.name} value={crop.id} />
+                        ))}
+                    </Picker>
+                </View>
+                <View style={styles.totalTaskContainer}>
+                <Text style={styles.PickerTitles}>Task Type</Text>
                 <Picker
                     selectedValue={taskData.TaskType}
                     onValueChange={value => handleChange('TaskType', value)}
@@ -135,8 +140,8 @@ const TodoEntryModal = ({ visible, onClose, onSave, taskID, farmers, locations, 
                         <Button title="Add Task Type" onPress={handleAddTaskType} />
                     </View>
                 )}
-
-                <Text>Comments</Text>
+                </View>
+                <Text >Comments</Text>
                 <TextInput
                     value={taskData.Comments}
                     onChangeText={text => handleChange('Comments', text)}
@@ -189,7 +194,7 @@ const TodoEntryModal = ({ visible, onClose, onSave, taskID, farmers, locations, 
                         style={styles.iconPicker}
                     >
                         <Picker.Item label="Select Icon" value="" />
-                        <Picker.Item label="Watering Can" value="watering-can" />
+                        <Picker.Item label="Watering Can" value="watering-can"/>
                         <Picker.Item label="Calendar" value="calendar" />
                         <Picker.Item label="Rake" value="rake" />
                         <Picker.Item label="Shovel" value="shovel" />
@@ -197,9 +202,14 @@ const TodoEntryModal = ({ visible, onClose, onSave, taskID, farmers, locations, 
                     </Picker>
                 </View>
 
-                <View style={styles.buttonContainer}>
-                    <Button title="Save" onPress={handleSave} />
-                    <Button title="Cancel" onPress={onClose} />
+                    <View style={styles.buttonContainer}>
+                        <Button
+                            buttonStyle={{ backgroundColor: 'green' }}
+                            title="Cancel" onPress={onClose} />
+                        <Button
+                            buttonStyle={{ backgroundColor: 'green' }}
+                            title="Save" onPress={handleSave} />
+                    
                     </View>
                 </View>
             </ScrollView>
@@ -208,55 +218,134 @@ const TodoEntryModal = ({ visible, onClose, onSave, taskID, farmers, locations, 
 };
 
 const styles = StyleSheet.create({
+    //pickerItemStyle: {
+   //     borderWidth: 1,
+      //  borderColor: 'black',
+    //    borderRadius: 5,
+    //    backgroundColor: Colors.PERIWINKLE_GRAY,
+    //    fontFamily: 'Domine-Regular',
+
+   // },
+    totalTaskContainer: {
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 5,
+        alignSelf: 'center',
+        width: '90%',
+        height: '12%',
+        marginHorizontal: '10%',
+        backgroundColor: Colors.PERIWINKLE_GRAY,
+        marginBottom: 10,
+        paddingHorizontal: 10
+
+    },
+    taskIDStyle: {
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 15,
+        fontFamily: 'Domine-Regular',
+        backgroundColor: 'white',
+        width: 'auto',
+        //alignSelf: 'center'
+    },
+    pickerTitles: {
+        fontFamily: 'Domine-Regular',
+        fontSize: 20,
+        alignSelf: 'center'
+
+    },
     optionsContainer: {
         //flexDirection: 'row'
-        backgroundColor: 'pink'
+        backgroundColor: 'pink',
+        fontFamily: 'Domine-Regular',
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 5
 
     },
     attributesContainer: {
-        flexDirection: 'row'
-
+       //r flexDirection: 'row'
+        backgroundColor: Colors.SCOTCH_MIST_TAN,
+        fontFamily: 'Domine-Regular',
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 5,
+        //paddingBottom:20,
+        height: '80%'
     },
 
     modalContainer: {
         flex: 1,
         padding: 20,
        // flexDirection: 'row'
+        fontFamily: 'Domine-Regular',
+        align: 'center',
+        backgroundColor: Colors.RIVER_BED
     },
     title: {
         fontSize: 24,
         fontWeight: 'bold',
         marginBottom: 20,
+        fontFamily: 'Domine-Regular',
+        alignSelf:'center'
     },
     input: {
         borderWidth: 1,
-        borderColor: '#ccc',
+        borderColor: 'black',
         borderRadius: 5,
-        padding: 10,
-        marginBottom: 15,
+        //padding: 10,
+        //marginBottom: 15,
+        fontFamily: 'Domine-Regular',
+        backgroundColor: 'white'
     },
     dateContainer: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        //justifyContent: 'space-between',
+        fontFamily: 'Domine-Regular',
+        //width: '90%',
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 5
     },
-    picker: {
-        height: 50,
-        width: 100,
+    picker: { // actuall picker part
+        height: 10,
+        width: '80%',
         marginBottom: 15,
+        fontFamily: 'Domine-Regular',
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 5, 
+        backgroundColor: 'white',
+        zIndex: 500, 
+        alignSelf: 'center',
+        paddingTop: 5
     },
     iconPickerContainer: {
-        alignItems: 'flex-end', // Aligns the icon picker to the right
-        flexDirection: 'row'
+        alignItems: 'flex-start', // Aligns the icon picker to the right
+        flexDirection: 'row',
+        fontFamily: 'Domine-Regular',
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 5
     },
     iconPicker: {
         height: 50,
-        width: 200,
-        marginBottom: 15,
+        width: 500,
+        //marginBottom: 15,
+        fontFamily: 'Domine-Regular',
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 5,
+        display: 'inline'
     },
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         marginTop: 20,
+        fontFamily: 'Domine-Regular',
+        backgroundColor: Colors.ALMOND_TAN,
     },
 });
 
