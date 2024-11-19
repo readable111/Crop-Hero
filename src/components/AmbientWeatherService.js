15b3a0d03023d1f48e91644c5c69da0f5829364d
Moreno, Daniel <danielmoreno3@my.unt.edu>
2024-11-19 12:20:15 -0600

    Finish up Ambient Weather implementation

--- src/components/AmbientWeatherService.js
+++ src/components/AmbientWeatherService.js
@@ -1,6 +1,16 @@
 import axios from 'axios';
 
+//Use the following URL to get a list of all devices:
+//https://private-anon-f9255d0157-ambientweather.apiary-proxy.com/v1/devices?apiKey=1&applicationKey=2
+
+//List of fields that MAY be returned depending on the device
+//https://github.com/ambient-weather/api-docs/wiki/Device-Data-Specs
+
+//List of used URLs
 const BASE_URL = 'https://api.ambientweather.net/v1';
+//Hex code for mock server may change over time as it is a free service provided by Apiary for Ambient Weather subscribers
+//To Update: visit https://ambientweather.docs.apiary.io/, Switch to Console, select Query Device Data, provide necessary params, set to Mock Server, Call Resource, copy the base URL in the listed call
+const BASE_MOCK_URL = 'https://private-anon-f9255d0157-ambientweather.apiary-mock.com/v1' 
 
 //Timestamp of last request
 //Must be global
@@ -31,7 +41,24 @@ const fetchWeatherData = rateLimiter(async (apiKey, appKey, deviceMacAddress) =>
         },
       }
     );
-    return response.data;
+
+    //this if is only true if the GET succeeded (so app/api keys are valid & so is MAC) but no data was returned by the device
+    if (!Array.isArray(response.data) || !response.data.length) {
+      console.log("Mock called because device not reporting")
+      const mockResponse = await axios.get(
+        `${BASE_MOCK_URL}/devices/${encodeURIComponent(deviceMacAddress)}`,
+        {
+          params: {
+            apiKey: apiKey,
+            applicationKey: appKey,
+          },
+        }
+      );
+      return mockResponse.data
+    } else{ 
+      return response.data;
+    }
+    
   } catch (error) {
     throw error;
   }
